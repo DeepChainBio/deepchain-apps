8ea8496d141958acef5fd2035cbee5564cf12285
Kevin Eloff <k.eloff@hotmail.co.za>
2021-04-16 12:26:07 +0200

    implement pytorch app

--- example/pytorch_app/src/app.py
+++ example/pytorch_app/src/app.py
@@ -10,8 +10,10 @@ from typing import Dict, List, Optional
 
 from deepchain.components import DeepChainApp, Transformers
 from tensorflow.keras.models import load_model
-from catboost import CatBoostRegressor, Pool
 import numpy as np
+
+import torch
+import torch.nn as nn
 
 from os import path
 import urllib
@@ -31,10 +33,8 @@ class App(DeepChainApp):
     def __init__(self, device: str = "cuda:0"):
         self._device = device
 
-        self.model = CatBoostRegressor(iterations=4000, score_function='L2',
-                                                depth=9, min_data_in_leaf=1,
-                                                learning_rate=0.04, 
-                                                loss_function='RMSE')
+        self.model = Model().to(self._device)
+
         # create dictionary to map Amino acids to integer
         self.i2s = [
             'PAD', 'EOS', 'P', 'V', 'I', 'K', 'N', 'B', 'F', 'Y', 'E', 'W', 'R', 
@@ -45,17 +45,10 @@ class App(DeepChainApp):
         self.i2i_bigram = np.arange(len(self.i2s)**2).reshape((len(self.i2s), len(self.i2s))) 
 
         # Make sure to put your checkpoint file in your_app/checkpoint folder
-        self._checkpoint_filename: Optional[str] = "model.cat"
+        self._checkpoint_filename: Optional[str] = "model.pth"
 
         if self._checkpoint_filename is not None:
-            # Download model if missing from checkpoint folder.
-            if not path.exists(Path(__file__).parent/'..'/'checkpoint'/f'{self._checkpoint_filename}'):
-                urllib.request.urlretrieve(
-                    "https://github.com/KevinEloff/deep-chain-apps/releases/download/1.0.0/model.cat",
-                    Path(__file__).parent/'..'/'checkpoint'/f'{self._checkpoint_filename}'
-                )
-
-            self.model.load_model(self.get_checkpoint_path(__file__))
+            self.model.load_state_dict(torch.load(self.get_checkpoint_path(__file__)))
 
     @staticmethod
     def score_names() -> List[str]:
@@ -75,12 +68,10 @@ class App(DeepChainApp):
         if not isinstance(sequences, list):
             sequences = [sequences]
 
-        for seq in sequences:
-            if len(seq) != 221: raise AssertionError("Only sequences of length 221 allowed for this model")
-
         test_data = np.stack([self._preprocess_seq(s) for s in sequences], axis=0)
-        test_pool = Pool(test_data, cat_features=np.arange(221 + (221 if WITH_BIGRAM else 0) + (221 if WITH_BIGRAM_O1 else 0)))
-        test_preds = self.model.predict(test_pool)
+        test_preds = []
+        for item in test_data:
+            with torch.no_grad(): test_preds.append(self.model(torch.tensor(item[None], device=self._device)).item())
 
         pred_list = [{self.score_names()[0]: pred} for pred in test_preds]
 
@@ -100,8 +91,44 @@ class App(DeepChainApp):
             bigrams_o1 = self.i2i_bigram[fr, fr_r2].tolist()
         else: bigrams_o1 = []
         
-        inference_item = np.array(full_cdr + bigrams + bigrams_o1)
+        inference_item = np.array([
+            full_cdr, 
+            bigrams, 
+            bigrams_o1
+        ])
         return inference_item
+
+class Model(nn.Module):
+
+    def __init__(self):
+        super(Model, self).__init__()
+        self.embed_plain = nn.Embedding(25, 128)
+        self.embed_bigram = nn.Embedding(25**2, 128)
+        self.embed_o_bigram = nn.Embedding(25**2, 128)
+        
+        self.BiLSTM = nn.LSTM(128*3, 256, num_layers=3, batch_first=True, bidirectional=True)
+
+        self.conv_full = nn.Sequential(nn.Conv1d(128*3, 128, 3, stride=1),
+                                       nn.Conv1d(128, 128, 3, stride=1),
+                                       nn.Conv1d(128, 256, 3, stride=1),
+                                       nn.SELU())
+        
+        self.hidden_linear = nn.Sequential(nn.Linear(256*2+256, 64), nn.SELU())
+        self.out_linear = nn.Linear(64, 1)
+
+    def forward(self, x):
+        embed = torch.cat((
+            self.embed_plain(x[:,0]), 
+            self.embed_bigram(x[:,1]), 
+            self.embed_o_bigram(x[:,2])
+        ), dim=-1)
+                                           
+        xn, (hn, cn) = self.BiLSTM(embed)
+        cn = torch.max(self.conv_full(embed.transpose(-1,-2)), dim=-1)[0]
+        
+        conc = torch.cat((xn[:,-1], cn), dim=1)
+        out = self.out_linear(self.hidden_linear(conc))
+        return out
 
 if __name__ == "__main__":
 
@@ -109,6 +136,6 @@ if __name__ == "__main__":
         ("QVMLKESGPGLVAPSGSLSITCTVLGFLLDSNGVHWVRQPPGKGLEWLGVIWAGGNTNYNSALMSRVSISKDNSKAQVFLKMKSLQTDDTANYYCARDFYAYDYFYYAMDYWGQGTSVTVSSAFTTPPSVYPLAPGSAAQTNSMVTLGCLVKGYFPEPVTVTWNSGSLSSGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVP"),
         ("QVQLKESGPGLVAPQQSLSITCTVSGFLLGTNGVHWVRQPPGKGLEWLGVIWAGGISNYNSALMSRVSISKDNSKSQVFLNMKSLQTDDTAMYYCARDFYDYDVFYYAMDYWGQGTSVTVSSAKTTPPSVYPLAPGSAAQTNSMVTLGCLVKGYFPEPVTVTWNSGSLSSGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVP"),
     ]
-    app = App("cpu")
+    app = App()
     scores = app.compute_scores(sequences)
     print(scores)
