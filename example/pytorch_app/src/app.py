77cc1ef0610432b34226d0a021d73049be6a3474
aurelien delfosse <a.delfosse@instadeep.com>
2021-04-19 09:57:56 +0200

    merge app

--- example/pytorch_app/src/app.py
+++ example/pytorch_app/src/app.py
@@ -4,25 +4,21 @@ The model scores the binding energy per position in the receptor binding domain 
 By Matthew Baas and Kevin Eloff
 """
 
-
-from pathlib import Path
+import urllib
+from os import path
 from typing import Dict, List, Optional
 
-from deepchain.components import DeepChainApp, Transformers
-from tensorflow.keras.models import load_model
 import numpy as np
-
 import torch
 import torch.nn as nn
-
-from os import path
-import urllib
+from deepchain.components import DeepChainApp
 
 Score = Dict[str, float]
 ScoreList = List[Score]
 
 WITH_BIGRAM = True
 WITH_BIGRAM_O1 = True
+
 
 class App(DeepChainApp):
     """
@@ -37,12 +33,37 @@ class App(DeepChainApp):
 
         # create dictionary to map Amino acids to integer
         self.i2s = [
-            'PAD', 'EOS', 'P', 'V', 'I', 'K', 'N', 'B', 'F', 'Y', 'E', 'W', 'R', 
-            'D', 'X', 'S', 'C', 'U', 'Q', 'A', 'M', 'H', 'L', 'G', 'T'
+            "PAD",
+            "EOS",
+            "P",
+            "V",
+            "I",
+            "K",
+            "N",
+            "B",
+            "F",
+            "Y",
+            "E",
+            "W",
+            "R",
+            "D",
+            "X",
+            "S",
+            "C",
+            "U",
+            "Q",
+            "A",
+            "M",
+            "H",
+            "L",
+            "G",
+            "T",
         ]
-        self.s2i = { k:v for k , v in zip(self.i2s, range(0,len(self.i2s)))}
+        self.s2i = {k: v for k, v in zip(self.i2s, range(0, len(self.i2s)))}
         # FIRST axis is the FROM part, 2nd axis is the 2nd item in bigram
-        self.i2i_bigram = np.arange(len(self.i2s)**2).reshape((len(self.i2s), len(self.i2s))) 
+        self.i2i_bigram = np.arange(len(self.i2s) ** 2).reshape(
+            (len(self.i2s), len(self.i2s))
+        )
 
         # Make sure to put your checkpoint file in your_app/checkpoint folder
         self._checkpoint_filename: Optional[str] = "model.pth"
@@ -55,7 +76,7 @@ class App(DeepChainApp):
         """
         Return a list of app score names
         """
-        return ["binding energy"]
+        return ["binding_energy"]
 
     def compute_scores(self, sequences: List[str]) -> ScoreList:
         """
@@ -65,13 +86,16 @@ class App(DeepChainApp):
                 - key of dict are score_names
         sequence must be 221 length proteins
         """
-        if not isinstance(sequences, list):
+        if not isinstance(sequences, List):
             sequences = [sequences]
 
         test_data = [self._preprocess_seq(s) for s in sequences]
         test_preds = []
         for item in test_data:
-            with torch.no_grad(): test_preds.append(self.model(torch.tensor(item[None], device=self._device)).item())
+            with torch.no_grad():
+                test_preds.append(
+                    self.model(torch.tensor(item[None], device=self._device)).item()
+                )
 
         pred_list = [{self.score_names()[0]: pred} for pred in test_preds]
 
@@ -80,61 +104,71 @@ class App(DeepChainApp):
     def _preprocess_seq(self, sequence: str) -> np.ndarray:
         full_cdr = list(sequence)
         full_cdr = [self.s2i[c] for c in full_cdr]
-        
+
         if WITH_BIGRAM:
             fr = np.array(full_cdr)
             fr_r = np.roll(fr, 1)
             bigrams = self.i2i_bigram[fr, fr_r].tolist()
-        else: bigrams = []
+        else:
+            bigrams = []
         if WITH_BIGRAM_O1:
             fr_r2 = np.roll(fr, 2)
             bigrams_o1 = self.i2i_bigram[fr, fr_r2].tolist()
-        else: bigrams_o1 = []
-        
-        inference_item = np.array([
-            full_cdr, 
-            bigrams, 
-            bigrams_o1
-        ])
+        else:
+            bigrams_o1 = []
+
+        inference_item = np.array([full_cdr, bigrams, bigrams_o1])
         return inference_item
 
-class Model(nn.Module):
 
+class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.embed_plain = nn.Embedding(25, 128)
-        self.embed_bigram = nn.Embedding(25**2, 128)
-        self.embed_o_bigram = nn.Embedding(25**2, 128)
-        
-        self.BiLSTM = nn.LSTM(128*3, 256, num_layers=3, batch_first=True, bidirectional=True)
+        self.embed_bigram = nn.Embedding(25 ** 2, 128)
+        self.embed_o_bigram = nn.Embedding(25 ** 2, 128)
 
-        self.conv_full = nn.Sequential(nn.Conv1d(128*3, 128, 3, stride=1),
-                                       nn.Conv1d(128, 128, 3, stride=1),
-                                       nn.Conv1d(128, 256, 3, stride=1),
-                                       nn.SELU())
-        
-        self.hidden_linear = nn.Sequential(nn.Linear(256*2+256, 64), nn.SELU())
+        self.BiLSTM = nn.LSTM(
+            128 * 3, 256, num_layers=3, batch_first=True, bidirectional=True
+        )
+
+        self.conv_full = nn.Sequential(
+            nn.Conv1d(128 * 3, 128, 3, stride=1),
+            nn.Conv1d(128, 128, 3, stride=1),
+            nn.Conv1d(128, 256, 3, stride=1),
+            nn.SELU(),
+        )
+
+        self.hidden_linear = nn.Sequential(nn.Linear(256 * 2 + 256, 64), nn.SELU())
         self.out_linear = nn.Linear(64, 1)
 
     def forward(self, x):
-        embed = torch.cat((
-            self.embed_plain(x[:,0]), 
-            self.embed_bigram(x[:,1]), 
-            self.embed_o_bigram(x[:,2])
-        ), dim=-1)
-                                           
+        embed = torch.cat(
+            (
+                self.embed_plain(x[:, 0]),
+                self.embed_bigram(x[:, 1]),
+                self.embed_o_bigram(x[:, 2]),
+            ),
+            dim=-1,
+        )
+
         xn, (hn, cn) = self.BiLSTM(embed)
-        cn = torch.max(self.conv_full(embed.transpose(-1,-2)), dim=-1)[0]
-        
-        conc = torch.cat((xn[:,-1], cn), dim=1)
+        cn = torch.max(self.conv_full(embed.transpose(-1, -2)), dim=-1)[0]
+
+        conc = torch.cat((xn[:, -1], cn), dim=1)
         out = self.out_linear(self.hidden_linear(conc))
         return out
+
 
 if __name__ == "__main__":
 
     sequences = [
-        ("QVMLKESGPGLVAPSGSLSITCTVLGFLLDSNGVHWVRQPPGKGLEWLGVIWAGGNTNYNSALMSRVSISKDNSKAQVFLKMKSLQTDDTANYYCARDFYAYDYFYYAMDYWGQGTSVTVSSAFTTPPSVYPLAPGSAAQTNSMVTLGCLVKGYFPEPVTVTWNSGSLSSGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVP"),
-        ("QVQLKESGPGLVAPQQSLSITCTVSGFLLGTNGVHWVRQPPGKGLEWLGVIWAGGISNYNSALMSRVSISKDNSKSQVFLNMKSLQTDDTAMYYCARDFYDYDVFYYAMDYWGQGTSVTVSSAKTTPPSVYPLAPGSAAQTNSMVTLGCLVKGYFPEPVTVTWNSGSLSSGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVP"),
+        (
+            "QVMLKESGPGLVAPSGSLSITCTVLGFLLDSNGVHWVRQPPGKGLEWLGVIWAGGNTNYNSALMSRVSISKDNSKAQVFLKMKSLQTDDTANYYCARDFYAYDYFYYAMDYWGQGTSVTVSSAFTTPPSVYPLAPGSAAQTNSMVTLGCLVKGYFPEPVTVTWNSGSLSSGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVP"
+        ),
+        (
+            "QVQLKESGPGLVAPQQSLSITCTVSGFLLGTNGVHWVRQPPGKGLEWLGVIWAGGISNYNSALMSRVSISKDNSKSQVFLNMKSLQTDDTAMYYCARDFYDYDVFYYAMDYWGQGTSVTVSSAKTTPPSVYPLAPGSAAQTNSMVTLGCLVKGYFPEPVTVTWNSGSLSSGVHTFPAVLQSDLYTLSSSVTVPSSTWPSETVTCNVAHPASSTKVDKKIVP"
+        ),
     ]
     app = App()
     scores = app.compute_scores(sequences)
